080c182af2e216e55c83fa7f645e95ec53dc57a8
Camren Chraplak <cchraplak@wpi.edu>
2025-10-14 19:57:23 -0400

    Added unit testing suite

---
+++ unity-app/main/test_app_main.c
@@ -0,0 +1,40 @@
+/*
+	test_app_main.c - testing entry point for code
+	Copyright (C) 2025 Camren Chraplak
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <unity.h>
+#include <full_test.h>
+
+TEST_CASE("NVM Tests", "[nvm]") {
+	testNVM();
+}
+
+TEST_CASE("Timer Tests", "[timer]") {
+	testTimers();
+}
+
+void setUp(void) {
+	// set stuff up here
+}
+
+void tearDown(void) {
+	// clean stuff up here
+}
+
+void app_main(void) {
+    unity_run_menu();
+}
